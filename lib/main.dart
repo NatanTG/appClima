f3a155d4e37bd33f1d7abb51e8d1119cf774a5cb
natan <natatidioligirotto@gmail.com>
2022-11-08 23:29:40 -0300

    conectando API

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:appclima/widgets/appclima_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
 
-void main() => runApp(appclima()); 
-
+Future main() async {
+  await DotEnv.load(fileName: '.env');
+  runApp(appclima());
+}
